95fb71bf60fc49d8f3ab8cae497acc7f6112a348
Tohyo <k.baujard@orange.fr>
2023-06-12 21:06:52 +0200

    mock http client on unit test

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -3,16 +3,47 @@
 namespace Tohyo\OpenGraphBundle\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
+use Tohyo\OpenGraphBundle\Dto\OpenGraphData;
 use Tohyo\OpenGraphBundle\OpenGraph;
 
 class UnitTest extends TestCase
 {
     public function testUnit()
     {
+        $response = new MockResponse(
+            <<<'HTML'
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta property="og:title" content="website-title">
+    <meta property="og:url" content="website-url">
+    <meta property="og:type" content="website-type">
+    <meta property="og:image" content="website-image">
+    <meta property="og:description" content="website-description">
+    <meta property="og:locale" content="website-locale">
+    <meta property="og:nook" content="nook">
+</head>
+    <body>
+        
+    </body>
+</html>
+HTML
+        );
 
-        $openGraphService = new OpenGraph(HttpClient::create());
-        dd($openGraphService->getData('https://stackoverflow.com/questions/10771248/how-to-remove-a-substring-from-a-string-using-php'));
-        dd($openGraphService->getData('https://symfony.com/blog/symfony-ux-2-0-and-stimulus-3-support'));
+        $openGraphService = new OpenGraph(new MockHttpClient($response));
+        $openGraphData = $openGraphService->getData('url');
+
+        $this->assertInstanceOf(OpenGraphData::class, $openGraphData);
+
+        $this->assertSame('website-title', $openGraphData->title);
+        $this->assertSame('website-url', $openGraphData->url);
+        $this->assertSame('website-type', $openGraphData->type);
+        $this->assertSame('website-image', $openGraphData->image);
+        $this->assertSame('website-description', $openGraphData->description);
+        $this->assertSame('website-locale', $openGraphData->locale);
+        $this->assertArrayHasKey('nook', $openGraphData->others);
+        $this->assertContains('nook', $openGraphData->others);
     }
 }
