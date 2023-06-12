f00a974eb394a05561ad6bbb77d40f14e01eac74
Tohyo <kevin@codekraft.fr>
2023-06-12 21:21:13 +0200

    refactor service

--- src/OpenGraph.php
+++ src/OpenGraph.php
@@ -8,26 +8,41 @@ use Tohyo\OpenGraphBundle\Dto\OpenGraphData;
 
 class OpenGraph
 {
+    private const OG_XPATH = '//meta[contains(@property, "og:")]';
+
+    private OpenGraphData $openGraphData;
+
     public function __construct(
         public HttpClientInterface $client
     ) {
+        $this->openGraphData = new OpenGraphData();
     }
 
     public function getData(string $url): OpenGraphData
     {
-        $openGraphData = new OpenGraphData();
-
         $crawler = new Crawler($this->client->request('GET', $url)->getContent());
-        $crawler->filterXPath('//meta[contains(@property, "og:")]')->each(function (Crawler $node) use ($openGraphData) {
-            if (property_exists($openGraphData, str_replace('og:', '',$node->attr('property')))) {
-                $openGraphData->{str_replace('og:', '',$node->attr('property'))} = $node->attr('content');
-            } else {
-                $openGraphData->others = [
-                    str_replace('og:', '', $node->attr('property')) => $node->attr('content')
-                ];
-            }
+        $crawler->filterXPath(self::OG_XPATH)->each(function (Crawler $node) {
+
+            $this->buildOpenGraphData(
+                $this->sanitizePropertyName($node->attr('property')),
+                $node->attr('content')
+            );
         });
 
-        return $openGraphData;
+        return $this->openGraphData;
+    }
+
+    private function buildOpenGraphData(string $propertyName, string $propertyValue): void
+    {
+        if (property_exists($this->openGraphData, $propertyName)) {
+            $this->openGraphData->{$propertyName} = $propertyValue;
+        } else {
+            $this->openGraphData->others = [$propertyName => $propertyValue];
+        }
+    }
+
+    private function sanitizePropertyName(string $property): string
+    {
+        return str_replace('og:', '', $property);
     }
 }
